11c4910e629d895ae9412b08cb1d59fb65bbdf63
Pandurnag <96949058+Pandurnag@users.noreply.github.com>
2022-09-28 18:51:24 +0530

    third commit

--- src/app/-helpers/data.service.ts
+++ src/app/-helpers/data.service.ts
@@ -12,7 +12,7 @@ export class DataService implements InMemoryDbService {
   createDb() {
     let users: User[] = [
       { id: 1, title: 'Mr', firstName: 'Pandurang', lastName: 'Hande', dob: '1989-08-01', email: 'panduranghande502@gmail.com', password: 'Pandu@1234', acceptTerms: true },
-      { id: 2, title: 'Mr', firstName: 'Vaijenath', lastName: 'Hande', dob: '19893-08-01', email: 'vaijenathhande502@gmail.com', password: '123456', acceptTerms: true }
+      { id: 2, title: 'Mr', firstName: 'Vaijenath', lastName: 'Hande', dob: '1993-08-01', email: 'vaijenathhande502@gmail.com', password: '123456', acceptTerms: true }
     ];
 
     return { users };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import Swal from 'sweetalert2';
+import { DBOperation } from './-helpers/db-operations';
+import { User } from './-helpers/user.interface';
 import { UserService } from './-helpers/user.service';
 
 @Component({
@@ -13,39 +15,106 @@ export class AppComponent implements OnInit {
   title = 'regisrationapp';
   //registerForm: FormGroup = new FormGroup({});
   registerForm: FormGroup
+  users: User[] = [];
+  submitted: boolean = false;
 
-  constructor(private _toastr: ToastrService, private _fb: FormBuilder, private_userService: UserService) {
+  buttonText: string = "Submit";
+  dbops: DBOperation;
+
+  constructor(private _toastr: ToastrService, private _fb: FormBuilder, private _userservice: UserService) {
 
   }
 
   ngOnInit() {
     this.setFromState();
+    this.getAllUsers();
 
   }
 
   setFromState() {
+    this.buttonText = "submit";
+    this.dbops = DBOperation.create;
+  }
     this.registerForm = this._fb.group({
-      id: [0],
-      title: ['', Validators.required],
-      firstName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
-      lastName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
-      email: ['', Validators.compose([Validators.required, Validators.email])],
-      dob: ['', Validators.compose([Validators.required, Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)])],
-      password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
-      confirmPassword: ['', Validators.required],
-      acceptTerms: [false, Validators.required]
+    id: [0],
+    title: ['', Validators.required],
+    firstName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+    lastName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(10)])],
+    email: ['', Validators.compose([Validators.required, Validators.email])],
+    dob: ['', Validators.compose([Validators.required, Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)])],
+    password: ['', Validators.compose([Validators.required, Validators.minLength(6)])],
+    confirmPassword: ['', Validators.required],
+    acceptTerms: [false, Validators.required]
 
-    });
+  });
 
   }
-  onSubmit() {
 
-    if (this.registerForm.invalid) {
-      return
-    }
+  get f(){
+  return this.registerForm.contrlos;
+}
+
+onSubmit() {
+  this.submitted = true;
+
+  console.log()
+
+  if (this.registerForm.invalid) {
+    return
+  }
+
+  switch (this.dbops) {
+    case DBOperation.create:
+      this._userservice.addUser(this.registerForm.value).subscribe(res =>){
+        this.toastr.success("User Added !!", "User Registration");
+        this.getAllUsers();
+        this.onCancel();
+      });
+      break;
+    case DBOperation.update:
+      this._userservice.updateUser(this.registerForm.value).subscribe(res =>){
+        this.toastr.success("User Updated !!", "User Registration");
+        this.getAllUsers();
+        this.onCancel();
+      });
+
+      break;
+
   }
 
   onCancel() {
     this.registerForm.reset();
+    this.buttonText = "Submit";
+    this.dbops = DBOperation.create;
+    this.submitted = false;
   }
+
+  getAllUsers() {
+    this._userservice.getUsers().subscribe((res: User[]) => {
+      this.users = res;
+      console.log(res);
+
+    });
+
+  }
+
+  Edit(userId: number) {
+    this.buttonText = "update";
+    this.dbops = DBOperation.update;
+
+    let user = this.users.find((u: User) => u.id === userId);
+    this.registerForm.patchValue(user);
+
+  }
+
 }
+Delete(userId: number) {
+  this._userservice.deleteUser(userId).subscribe(res => {
+    this.getAllUsers();
+    this._toastr.success("Deleted Sucess !!", "User Registration");
+  });
+}
+function f() {
+  throw new Error('Function not implemented.');
+}
+
